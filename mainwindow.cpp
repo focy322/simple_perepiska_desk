fe145c6eb204513cd8e89c1711fc5f8ee8f5ccc6
Марк Беляков <belyackov.mark@mail.ru>
2026-03-03 00:47:47 +0300

    Создание базового интерфейса и симуляции отправки сообщений
    Создание интерфейса (список чатов, окно сообщений ) и симуляция отправки сообщений (через искусственную хэш-таблицу)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,39 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , chatsListModel(new QStringListModel(this))
+    , messagesListModel(new QStringListModel(this))
+    , chatMessages({{"Chat 1", {"Привет", "Это чат 1"}},
+                    {"Chat 2", {"Привет", "Это чат 2"}},
+                    {"Chat 3", {"Привет", "Это чат 3"}}})
 {
     ui->setupUi(this);
+    ui->chatsView->setStyleSheet(
+        "QListView {"
+        " background-color:#FFF8DC;"
+        " border: none;"
+        " outline: 0; "
+        " border-right: 1px solid black;"
+        " }"
+        "QListView::item {"
+        " min-height: 36px;"
+        " padding: 8px 12px;"
+        " border-radius: 6px;"
+        " }"
+        "QListView::item:hover {"
+        " background-color:#FFE4B5;"
+        " }"
+        "QListView::item:selected {"
+        " background-color:#F4A460;"
+        " color:black;"
+        " }");
+    chatsListModel->setStringList({"Chat 1", "Chat 2", "Chat 3"});
+    ui->chatsView->setModel(chatsListModel);
+    ui->messagesView->setModel(messagesListModel);
 }
 
 MainWindow::~MainWindow()
@@ -12,4 +41,44 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+
+void MainWindow::on_chatsView_clicked(const QModelIndex &chatIndex)
+{
+    if (chatIndex.isValid())
+    {
+        QString chatName = chatIndex.data().toString(); // Название чата
+        ui->chatName->setText(chatName);
+        auto chatIt = chatMessages.constFind(chatName); // Итератор на список сообщений (QStringList) для чата с названием chatName
+        if (chatIt != chatMessages.constEnd()) {
+            messagesListModel->setStringList(chatIt.value());
+        } else {
+            messagesListModel->setStringList({});
+        }
+#ifdef QT_DEBUG
+        qDebug() << chatIndex.data();
+#endif
+    }
+}
+
+
+void MainWindow::on_sendMessage_clicked()
+{
+    QString msgToSend = ui->messageInput->text();
+    bool condToSendMsg = !msgToSend.isEmpty() && chatMessages.constFind(ui->chatName->text()) != chatMessages.constEnd(); // Условия для отправки сообщения
+    if (condToSendMsg)
+    {
+        auto chatIt = chatMessages.find(ui->chatName->text()); // Итератор на список сообщений (QStringList) для чата с названием chatName
+        chatIt.value().append(msgToSend);
+        messagesListModel->setStringList(chatIt.value());
+        ui->messageInput->clear();
+
+    }
+    else
+    {
+        ui->messageInput->clear();
+        return;
+    }
+
+}
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStringListModel>
+
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -17,8 +20,21 @@ public:
     ~MainWindow();
 
 private slots:
+    /**
+      * При выборе чата из списка чатов в messagesView (chatName) выводит его название
+      *
+      * @param chatIndex индекс чата из списка чатов chatsList
+      */
+    void on_chatsView_clicked(const QModelIndex &chatIndex);
+
+    void on_sendMessage_clicked();
 
 private:
     Ui::MainWindow *ui;
+    QStringListModel *chatsListModel;          //!< Модель для списков чатов для отображения
+    QStringListModel *messagesListModel;       //!< Модель для сообщений для конкретного чата
+    QHash<QString, QStringList> chatMessages;  //!< Хранилище сообщений по чатам
+    // TODO: добавить currentChatId/currentChatName как отдельное поле класса (а не брать из текста chatName)
+
 };
 #endif // MAINWINDOW_H
